902ae070d12e6ea9e67333574a0047ecf61d64ea
Logan Foster <logan@truepathvision.com>
2026-05-25 10:56:23 -0400

    updated config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugin.extras.ui.mini-animate" },
     -- Python
     { import = "lazyvim.plugins.extras.lang.python" },
     -- Java
